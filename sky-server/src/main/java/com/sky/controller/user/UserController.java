db73e46ef9ec23eeafe62e5c340ca75561a72595
questionviper <lr1811399255@qq.com>
2024-08-08 20:32:40 +0800

    用户登录和拦截器开发

---
+++ sky-server/src/main/java/com/sky/controller/user/UserController.java
@@ -0,0 +1,50 @@
+package com.sky.controller.user;
+
+import com.sky.constant.JwtClaimsConstant;
+import com.sky.dto.UserLoginDTO;
+import com.sky.entity.User;
+import com.sky.properties.JwtProperties;
+import com.sky.result.Result;
+import com.sky.service.UserService;
+import com.sky.utils.JwtUtil;
+import com.sky.vo.UserLoginVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/user/user")
+@Api("c端用户相关接口")
+@Slf4j
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private JwtProperties jwtProperties;
+
+    @PostMapping("/login")
+    @ApiOperation("用户登录")
+    public Result<UserLoginVO> Login(@RequestBody UserLoginDTO userLoginDTO) {
+        log.info("微信用户登录：{}", userLoginDTO);
+        User user = userService.wxLogin(userLoginDTO);
+
+        //生成jwt
+        Map<String,Object> claims = new HashMap<>();
+        claims.put(JwtClaimsConstant.USER_ID, user.getId());
+        String token = JwtUtil.createJWT(jwtProperties.getUserSecretKey(),jwtProperties.getUserTtl(),claims);
+
+        UserLoginVO loginVO = UserLoginVO.builder()
+                .id(user.getId())
+                .openid(user.getOpenid())
+                .token(token)
+                .build();
+        return Result.success(loginVO);
+    }
+}

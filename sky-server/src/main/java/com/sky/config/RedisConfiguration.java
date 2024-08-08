446c3a89c78d203868bdd358f856e3f38a60e9e4
questionviper <lr1811399255@qq.com>
2024-08-08 16:37:34 +0800

    店铺营业状态设置

---
+++ sky-server/src/main/java/com/sky/config/RedisConfiguration.java
@@ -0,0 +1,20 @@
+package com.sky.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@Slf4j
+public class RedisConfiguration {
+    public RedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate redisTemplate = new RedisTemplate();
+        //设置连接工厂对象
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        //设置key序列化系
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        return redisTemplate;
+    }
+}

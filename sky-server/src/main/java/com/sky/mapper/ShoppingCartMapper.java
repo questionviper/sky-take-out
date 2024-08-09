4ac915c2808cf85f2267d3501f788d1532c32663
questionviper <lr1811399255@qq.com>
2024-08-09 18:52:39 +0800

    视频Day9作业有关用户端订单的代码

--- sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
+++ sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -28,4 +28,6 @@ public interface ShoppingCartMapper {
 
     @Delete("delete from shopping_cart where id = #{id}")
     void deleteById(Long id);
+
+    void insertBatch(List<ShoppingCart> shoppingCartList);
 }
